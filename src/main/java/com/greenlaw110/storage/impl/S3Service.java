a9af31dadc339e7511195560bc159b7f25327129
Green Luo <greenlaw110@gmail.com>
2013-06-21 09:17:16 +1000

    s3 service: specify storage class in sobject

--- src/main/java/com/greenlaw110/storage/impl/S3Service.java
+++ src/main/java/com/greenlaw110/storage/impl/S3Service.java
@@ -19,14 +19,21 @@ import java.util.Map;
 public class S3Service extends StorageServiceBase implements IStorageService {
 
     public static enum StorageClass {
-        STANDARD, REDUCED_REDUNDANCY;
+        STANDARD, REDUCED_REDUNDANCY, GLACIER;
 
-        public static StorageClass valueOfIgnoreCase(String s) {
+        public static StorageClass valueOfIgnoreCase(String s, StorageClass def) {
+            if (S.empty(s)) {
+                return def;
+            }
             s = s.trim();
-            if ("rrs".equalsIgnoreCase(s) || "rr".equalsIgnoreCase(s) || "reduced_redundancy".equalsIgnoreCase(s) || "reducedRedundancy".equalsIgnoreCase(s)) {
+            if ("r".equalsIgnoreCase(s) || "rrs".equalsIgnoreCase(s) || "rr".equalsIgnoreCase(s) || "reduced_redundancy".equalsIgnoreCase(s) || "reducedRedundancy".equalsIgnoreCase(s)) {
                 return REDUCED_REDUNDANCY;
-            } else {
+            } else if ("glacier".equalsIgnoreCase(s) || "g".equalsIgnoreCase(s)) {
+                return GLACIER;
+            } else if ("s".equalsIgnoreCase(s) || "standard".equalsIgnoreCase(s)) {
                 return STANDARD;
+            } else {
+                return def;
             }
         }
     }
@@ -41,9 +48,9 @@ public class S3Service extends StorageServiceBase implements IStorageService {
      * For certain case for example, gallery application, it doesn't need the GET operation because end 
      * user can access the object directly from aws' static web service 
      */
-    public static final String CONF_NO_GET = "storage.s3.noGet";
+    public static final String CONF_GET_META_ONLY = "storage.s3.get.MetaOnly";
 
-    public static final String ATTR_STORAGE_CLASS = "storage-class";
+    public static final String ATTR_STORAGE_CLASS = "x-amz-storage-class";
 
 
     private String awsKeyId;
@@ -51,7 +58,7 @@ public class S3Service extends StorageServiceBase implements IStorageService {
     private StorageClass defStorageClass = StorageClass.REDUCED_REDUNDANCY;
     private String bucket;
     private String staticWebEndPoint = null;
-    private boolean noGet = false;
+    private boolean loadMetaOnly = false;
     
     
     public static AmazonS3 s3;
@@ -74,7 +81,7 @@ public class S3Service extends StorageServiceBase implements IStorageService {
         }
         String sc = conf.get(CONF_DEF_STORAGE_CLASS);
         if (null != sc) {
-            defStorageClass = StorageClass.valueOfIgnoreCase(sc);
+            defStorageClass = StorageClass.valueOfIgnoreCase(sc, defStorageClass);
         }
         bucket = conf.get(CONF_BUCKET);
         if (null == bucket) {
@@ -86,8 +93,8 @@ public class S3Service extends StorageServiceBase implements IStorageService {
         AWSCredentials cred = new BasicAWSCredentials(awsKeyId, awsKeySecret);
         s3 = new AmazonS3Client(cred);
 
-        if (conf.containsKey(CONF_NO_GET)) {
-            noGet = Boolean.parseBoolean(conf.get(CONF_NO_GET));
+        if (conf.containsKey(CONF_GET_META_ONLY)) {
+            loadMetaOnly = Boolean.parseBoolean(conf.get(CONF_GET_META_ONLY));
         }
     }
 
@@ -100,22 +107,27 @@ public class S3Service extends StorageServiceBase implements IStorageService {
 
     @Override
     public ISObject get(String key) {
-        if (noGet || S.empty(staticWebEndPoint)) {
-            return SObject.getDumpObject(key);
-        } else {
-            try {
+        try {
+            ISObject sobj = null;
+            ObjectMetadata meta;
+            if (!loadMetaOnly || S.empty(staticWebEndPoint)) {
                 GetObjectRequest req = new GetObjectRequest(bucket, key);
                 S3Object s3obj = s3.getObject(req);
-                ISObject sobj = SObject.valueOf(key, s3obj.getObjectContent());
-                ObjectMetadata meta = s3obj.getObjectMetadata();
-                Map<String, String> map = meta.getUserMetadata();
-                for (String k : map.keySet()) {
-                    sobj.setAttribute(k, map.get(k));
-                }
-                return sobj;
-            } catch (AmazonS3Exception e) {
-                return SObject.getInvalidObject(key, e);
+                sobj = SObject.valueOf(key, s3obj.getObjectContent());
+                meta = s3obj.getObjectMetadata();
+            } else {
+                GetObjectMetadataRequest req = new GetObjectMetadataRequest(bucket, key);
+                meta = s3.getObjectMetadata(req);
+                sobj = SObject.getDumpObject(key);
             }
+            
+            Map<String, String> map = meta.getUserMetadata();
+            for (String k : map.keySet()) {
+                sobj.setAttribute(k, map.get(k));
+            }
+            return sobj;
+        } catch (AmazonS3Exception e) {
+            return SObject.getInvalidObject(key, e);
         }
     }
 
@@ -129,6 +141,10 @@ public class S3Service extends StorageServiceBase implements IStorageService {
         meta.setUserMetadata(attrs); 
         
         PutObjectRequest req = new PutObjectRequest(bucket, key, stuff.asInputStream(), meta);
+        StorageClass storageClass = StorageClass.valueOfIgnoreCase(attrs.remove(ATTR_STORAGE_CLASS), defStorageClass);
+        if (null != storageClass) {
+            req.setStorageClass(storageClass.toString());
+        }
         req.withCannedAcl(CannedAccessControlList.PublicRead);
         s3.putObject(req);
     }
